ac77cc0633216a914f50630b1c731e53dcdc2586
sedhupathygit <85553808+sedhupathygit@users.noreply.github.com>
2022-06-29 13:17:40 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,23 @@
-const sodium = require('tweetsodium');
+const sodium=require('libsodium-wrappers')
+const secret = 'plain-text-secret' // replace with secret before running the script.
+const key = 'base64-encoded-public-key' // replace with the Base64 encoded public key.
 
-const key = "base64-encoded-public-key";
-const value = "plain-text-secret";
+//Check if libsodium is ready and then proceed.
 
-// Convert the message and key to Uint8Array's (Buffer implements that interface)
-const messageBytes = Buffer.from(value);
-const keyBytes = Buffer.from(key, 'base64');
+sodium.ready.then( ()=>{
 
-// Encrypt using LibSodium.
-const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+// Convert Secret & Base64 key to Uint8Array.
+let binkey= sodium.from_base64(key, sodium.base64_variants.ORIGINAL) //Equivalent of Buffer.from(key, 'base64')
+let binsec= sodium.from_string(secret) // Equivalent of Buffer.from(secret)
 
-// Base64 the encrypted secret
-const encrypted = Buffer.from(encryptedBytes).toString('base64');
+//Encrypt the secret using LibSodium
+let encBytes= sodium.crypto_box_seal(binsec,binkey) // Similar to tweetsodium.seal(binsec,binkey)
 
-console.log(encrypted);
+// Convert encrypted Uint8Array to Base64
+let output=sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL) //Equivalent of Buffer.from(encBytes).toString('base64')
+
+console.log(output)
+});
 
 module.exports = async ({github, context, core}) => {
 await github.request('PUT /repos/{owner}/{repo}/dependabot/secrets/{secret_name}', {
