f2bf818afa238b8ef10895fc3bef760f08ade528
sedhupathygit <85553808+sedhupathygit@users.noreply.github.com>
2022-06-29 13:10:47 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
+const sodium = require('tweetsodium');
+
+const key = "base64-encoded-public-key";
+const value = "plain-text-secret";
+
+// Convert the message and key to Uint8Array's (Buffer implements that interface)
+const messageBytes = Buffer.from(value);
+const keyBytes = Buffer.from(key, 'base64');
+
+// Encrypt using LibSodium.
+const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+
+// Base64 the encrypted secret
+const encrypted = Buffer.from(encryptedBytes).toString('base64');
+
+console.log(encrypted);
+
 module.exports = async ({github, context, core}) => {
 await github.request('PUT /repos/{owner}/{repo}/dependabot/secrets/{secret_name}', {
 owner: 'sedhupathygit',
